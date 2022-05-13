552c05e8ca070e3b7cb9e6b387a268fd18d2a8c2
Jesse <gojsse@gmail.com>
2022-05-13 07:39:04 -0500

    Add Project class, Listener type, readme, .nvmrc

---
+++ README.md
@@ -0,0 +1,12 @@
+# TS Demo
+Taken from Maximilian Schwarzmüller's (Understanding Typescript)[https://www.udemy.com/course/understanding-typescript/] course. Manage a list of projects using drag and drop.
+
+Typescript/Core Concepts:
+- Types: types, interface, enum
+- Decorators
+- Classes, methods (public, private, static), singleton class
+- Manually creating state management
+
+### Running/Watching the app
+`tsc -w`
+Will open app in a browser tab
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+// Project Type Class rather than interface in order to instatiate
+enum ProjectStatus { Active, Finished }
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {
+
+  }
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void
+
 class ProjectState {
-  private listeners: any[] = []
-  private projects: any[] = []
+  private listeners: Listener[] = []
+  private projects: Project[] = []
   private static instance: ProjectState
 
   // Make singleton for managing global state and watching for changes
@@ -17,17 +33,18 @@ class ProjectState {
     return this.instance
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn)
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople
-    }
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Active
+    )
     this.projects.push(newProject)
     for (const listenerFn of this.listeners) {
       // Copy not reference
@@ -99,7 +116,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLElement
-  assignedProjects: any[]
+  assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement
@@ -110,7 +127,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement
     this.element.id = `${this.type}-projects`
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects
       this.renderProjects()
     })
